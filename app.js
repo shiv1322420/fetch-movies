d0c0f43f8b5526788d26e12597ee817799d72f2c
Shiv Kumar <sk623520@gmail.com>
2019-11-04 12:30:31 +0530

    commit

--- app.js
+++ app.js
@@ -15,106 +15,104 @@ app.get('/results', function (req, res) {
     let option = req.query.searchOpt;
     let movieSearch = req.query.search;
     console.log(option)
-    if (option == 'movieId') {
-        let sqlQuery = `select * from movies where imdbID='${movieSearch}'`;
-        sqlConnect.query(sqlQuery, (err, rows, fields) => {
-            if (err) {
-                console.log(err.message);
-                res.render('results',{message:err.message})
-            }
-            else if (rows.length > 0) {
-                let movieArr=[];
-                let data = rows;
-                movieArr.push(data);
-                console.log(movieArr);
-                res.render('results',{movieArr:movieArr[0]})
+    try {
+        if (option == 'movieId') {
+            let sqlQuery = `select * from movies where imdbID='${movieSearch}'`;
+            sqlConnect.query(sqlQuery, (err, rows, fields) => {
+                if (err) {
+                    console.log(err.message);
+                    res.render('results', { error: err.message })
+                }
+                else if (rows.length > 0) {
+                    let movieArr = [];
+                    let data = rows;
+                    movieArr.push(data);
+                    console.log(movieArr);
+                    res.render('results', { movieArr: movieArr[0] })
 
-            }
-            else {
-                let query = req.query.search;
-                let movieArr=[];
-                console.log(req.query);
-                let url = 'https://www.omdbapi.com/?i=' + query + '&apikey=b60af205';
-                request(url, function (error, response, body) {
-                    if (!error && response.statusCode == 200) {
-                        var data = JSON.parse(body)
-                        movieArr.push(data);
-                        console.log(movieArr)
-                        res.render('results',{movieArr:movieArr})
-                    }
-                })
-            }
-        });
+                }
+                else {
+                    let query = req.query.search;
+                    let movieArr = [];
+                    console.log(req.query);
+                    let url = 'https://www.omdbapi.com/?i=' + query + '&apikey=b60af205';
+                    request(url, function (error, response, body) {
+                        if (!error && response.statusCode == 200) {
+                            var data = JSON.parse(body)
+                            movieArr.push(data);
+                            let resp = movieArr[0].Response;
+                            if (resp == "False") {
+                                console.log('-$$' + movieArr)
+                                res.json(
+                                    {
+                                        message: "not success",
+                                        data: movieArr,
+                                    })
+                            }
+                            console.log(" --" + movieArr[0].Response)
+                            res.render('results', { movieArr: movieArr })
+                        }
+                    })
+                }
+            });
 
+        }
+        else {
+
+            let sqlQuery = `select * from movies where Title like '%${movieSearch}%'`;
+            console.log(sqlQuery);
+            sqlConnect.query(sqlQuery, (err, rows, fields) => {
+                if (err) {
+                    console.log(err.message);
+                    throw err;
+                }
+                else if (rows.length > 0) {
+                    let data = rows;
+                    movieArr.push(data);
+                    console.log(movieArr);
+                    res.render('results', { movieArr: movieArr[0] })
+                }
+                else {
+                    let query = req.query.search;
+                    console.log(req.query);
+                    let movieArr = [];
+                    var url = 'http://www.omdbapi.com/?t=' + query + '&apikey=b60af205';
+                    console.log(url)
+                    request(url, function (error, response, body) {
+                        if (!error && response.statusCode == 200) {
+                            var data = JSON.parse(body)
+                            console.log("---body")
+                            console.log(body)
+                            console.log("---data")
+                            console.log(data)
+                            movieArr.push(data);
+                            let resp = movieArr[0].Response;
+                            if (resp == "False") {
+                                console.log('-$$' + movieArr)
+                                res.json(
+                                    {
+                                        message: "not success",
+                                        data: movieArr,
+                                    }
+                                )
+                            }
+                            res.render('results', { movieArr: movieArr })
+                        }
+                        else {
+                            throw error;
+                        }
+                    });
+                }
+            });
+
+        }
+    } catch (error) {
+        res.send(error);
     }
-    else {
 
-        let sqlQuery = `select * from movies where Title like '%${movieSearch}%'`;
-        console.log(sqlQuery);
-        sqlConnect.query(sqlQuery, (err, rows, fields) => {
-            if (err) {
-                console.log(err.message);
-            }
-            else if (rows.length > 0) {
-                let data = rows;
-                movieArr.push(data);
-                console.log(movieArr);
-                res.render('results',{movieArr:movieArr[0]})
-            }
-            else {
-                let query = req.query.search;
-                console.log(req.query);
-                let movieArr=[];
-                var url = 'http://www.omdbapi.com/?t=' + query + '&apikey=b60af205';
-                console.log(url)
-                request(url, function (error, response, body) {
-                    if (!error && response.statusCode == 200) {
-                        var data = JSON.parse(body)
-                        console.log("---body")
-                        console.log(body)
-                        console.log("---data")
-                        console.log(data)
-                        movieArr.push(data);
-                        res.render('results',{movieArr:movieArr})
-                    }
-                });
-            }
-        });
-
-    }
 
 });
 
-
-
-// app.get('/results', function(req, res){
-//     var query = req.query.search;
-//     var url = 'https://www.omdbapi.com/?s=' + query + '&apikey=b60af205';
-//     request(url, function(error, response, body){
-//         if(!error && response.statusCode == 200){
-//             var info = JSON.parse(body)
-//             //res.render('results', {data: data});
-
-//             res.status(200).json(
-//                 {
-//                    message:"success",
-//                    info
-//                 }
-//             )
-//         }
-//     });
-// });
-
-// app.get('/id', function(req, res){
-//     var query = req.query.movieId;
-//     var url = 'https://www.omdbapi.com/?i=' + query + '&apikey=b60af205';
-//     request(url, function(error, response, body){
-//         if(!error && response.statusCode == 200){
-//             var data = JSON.parse(body)
-//             res.render('results', {data: data});
-//         }
-//     });
-// });
 
 app.listen(3002, function () {
     console.log('Movie app started on port: 3002');
